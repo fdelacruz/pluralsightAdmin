7ea2ec0c678e4a324428b99baac375e1f46f2f5c
Francisco De La Cruz <helloquico@gmail.com>
2015-09-11 17:52:04 -0400

    Transitions

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -44432,6 +44432,21 @@ module.exports = NotFoundPage;
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+		statics: {
+			willTransitionTo: function(transition, params, query, callback) {
+				if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+						transition.about();	
+				} else {
+					callback();
+				}		
+			},
+
+			willTransitionFrom: function(transition, component) {
+				if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+						transition.about();	
+				} 
+			}
+		},
 
 		render: function() {
 			return (
