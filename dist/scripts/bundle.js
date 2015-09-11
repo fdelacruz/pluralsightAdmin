32538ad50747a1f833e93a061d42aa5d64ea1511
Francisco De La Cruz <helloquico@gmail.com>
2015-09-11 18:03:20 -0400

    Try HTML5 History Location Style URLs

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -44622,7 +44622,7 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-Router.run(routes, function(Handler) {
+Router.run(routes, Router.HistoryLocation, function(Handler) {
 	React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
 
