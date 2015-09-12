604a7094df76dfdae429aa51531bc4c253c19964
Francisco De La Cruz <helloquico@gmail.com>
2015-09-11 21:56:45 -0400

    Consume Reusable Inputs

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -44499,6 +44499,7 @@ module.exports = App;
 "use strict";
 
 var React = require('react');
+var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({displayName: "AuthorForm",
 
@@ -44506,25 +44507,17 @@ var AuthorForm = React.createClass({displayName: "AuthorForm",
 			return (
 					React.createElement("form", null, 
 						React.createElement("h1", null, "Manage Author"), 
-						React.createElement("label", {htmlFor: "firstName"}, "First Name"), 
-						React.createElement("input", {type: "text", 
+						React.createElement(Input, {
 								name: "firstName", 
-								className: "form-control", 
-								placeholder: "First Name", 
-								ref: "firstName", 
-								onChange: this.props.onChange, 
-								value: this.props.author.firstName}), 
-						React.createElement("br", null), 
+								label: "First Name", 
+								value: this.props.author.firstName, 
+								onChange: this.props.onChange}), 
 
-						React.createElement("label", {htmlFor: "lastName"}, "Last Name"), 
-						React.createElement("input", {type: "text", 
+						React.createElement(Input, {
 								name: "lastName", 
-								className: "form-control", 
-								placeholder: "Last Name", 
-								ref: "lastName", 
-								onChange: this.props.onChange, 
-								value: this.props.author.lastName}), 
-						React.createElement("br", null), 
+								label: "Last Name", 
+								value: this.props.author.lastName, 
+								onChange: this.props.onChange}), 
 
 						React.createElement("input", {type: "submit", value: "Save", className: "btn btn-default"})
 					)
@@ -44534,7 +44527,7 @@ var AuthorForm = React.createClass({displayName: "AuthorForm",
 
 module.exports = AuthorForm;
 
-},{"react":197}],204:[function(require,module,exports){
+},{"../common/textInput":208,"react":197}],204:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -44673,6 +44666,47 @@ module.exports = Header;
 
 var React = require('react');
 
+var Input = React.createClass({displayName: "Input",
+	propTypes: {
+		name: React.PropTypes.string.isRequired,
+		label: React.PropTypes.string.isRequired,
+		onChange: React.PropTypes.func.isRequired,
+		placeholder: React.PropTypes.string,
+		value: React.PropTypes.string,
+		error: React.PropTypes.string
+	},
+
+	render: function() {
+		var wrapperClass = 'form-group';
+		if (this.props.error && this.props.error.length > 0) {
+			wrapperClass += " " + 'has-error';
+		}
+
+		return (
+				React.createElement("div", {className: wrapperClass}, 
+					React.createElement("label", {htmlFor: this.props.name}, this.props.label), 
+					React.createElement("div", {className: "field"}, 
+						React.createElement("input", {type: "text", 
+							name: this.props.name, 
+							className: "form-control", 
+							placeholder: this.props.placeholder, 
+							ref: this.props.name, 
+							value: this.props.value, 
+							onChange: this.props.onChange}), 
+						React.createElement("div", {className: "input"}, this.props.error)
+					)
+				)
+		);
+	}
+});
+
+module.exports = Input;
+
+},{"react":197}],209:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+
 var Router = require('react-router');
 var Link = Router.Link;
 
@@ -44690,7 +44724,7 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":197,"react-router":28}],209:[function(require,module,exports){
+},{"react":197,"react-router":28}],210:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -44701,7 +44735,7 @@ Router.run(routes, function(Handler) {
 	React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
 
-},{"./routes":210,"react":197,"react-router":28}],210:[function(require,module,exports){
+},{"./routes":211,"react":197,"react-router":28}],211:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -44727,4 +44761,4 @@ var routes = (
 
 module.exports = routes;
 
-},{"./components/NotFoundPage":200,"./components/about/aboutPage":201,"./components/app":202,"./components/authors/authorPage":205,"./components/authors/manageAuthorPage":206,"./components/homePage":208,"react":197,"react-router":28}]},{},[209]);
+},{"./components/NotFoundPage":200,"./components/about/aboutPage":201,"./components/app":202,"./components/authors/authorPage":205,"./components/authors/manageAuthorPage":206,"./components/homePage":209,"react":197,"react-router":28}]},{},[210]);
--- src/components/common/textInput.js
+++ src/components/common/textInput.js
@@ -20,15 +20,15 @@ var Input = React.createClass({
 
 		return (
 				<div className={wrapperClass}>
-					<label htmlFor={this.props}>{this.props.label}</label>
+					<label htmlFor={this.props.name}>{this.props.label}</label>
 					<div className="field">
 						<input type="text"
 							name={this.props.name}
 							className="form-control"
 							placeholder={this.props.placeholder}
 							ref={this.props.name}
-							onChange={this.props.onChange}
-							value={this.props.author.firstName} />
+							value={this.props.value}
+							onChange={this.props.onChange} />
 						<div className="input">{this.props.error}</div>
 					</div>
 				</div>
