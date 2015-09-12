7d7cc5aa0c9eb3740e08aec71c647f9bac8c1251
Francisco De La Cruz <helloquico@gmail.com>
2015-09-12 08:04:20 -0400

    Use PropTypes to make expectations clear

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -4,6 +4,12 @@ var React = require('react');
 var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
+		propTypes: {
+			author: React.PropTypes.object.isRequired,
+			onSave: React.PropTypes.func.isRequired,
+			onChange: React.PropTypes.func.isRequired,
+			errors: React.PropTypes.object
+		},
 
 		render: function() {
 			return (
