831645408360a80cfd637727e74a4f41bad62269
Francisco De La Cruz <helloquico@gmail.com>
2015-09-12 19:04:42 -0400

    Stores - Private Storage

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -4,7 +4,10 @@ var Dispatcher = require('../dispatcher/appDispatcher');
 var ActionTypes = require('../constants/actionTypes');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+var _ = require('lodash');
 var CHANGE_EVENT = 'change';
+
+var _authors = [];
 
 var AuthorStore = assign({}, EventEmitter.prototype, {
 	addChangeListener: function(callback) {
@@ -17,12 +20,22 @@ var AuthorStore = assign({}, EventEmitter.prototype, {
 
 	emitChange: function() {
 		this.emit(CHANGE_EVENT);
+	},
+
+	getAllAuthors: function() {
+		return _authors;
+	},
+
+	getAuthorById: function(id) {
+		return _.find(_authors, {id: id});
 	}
 });
 
 Dispatcher.register(function(action) {
 	switch (action.actionType) {
-
+		case ActionTypes.CREATE_AUTHOR:
+				_authors.push(action.author);
+				AuthorStore.emitChange();
 	}
 });
 
