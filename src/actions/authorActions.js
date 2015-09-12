3299ce02e273226d4ba7e0bdfbf1bda9e375a75d
Francisco De La Cruz <helloquico@gmail.com>
2015-09-12 17:51:03 -0400

    Actions

---
+++ src/actions/authorActions.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var Dispatcher = require('../distpatcher/appDispatcher');
+var AuthorApi = require('../api/authorApi');
+var ActionTypes = require('../constants/actionTypes');
+
+var AuthorActions = {
+	createAuthor: function(author) {
+		var newAuthor = AuthorApi.saveAuthor(author);
+
+		// "Hey Dispatcher, go tell all the Stores that an author was just created."
+		Dispatcher.dispatch({
+			actionType: ActionTypes.CREATE_AUTHOR,
+			author: newAuthor
+		});
+	}
+};
+
+module.exports = AuthorActions;
