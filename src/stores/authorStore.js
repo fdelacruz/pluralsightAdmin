8ab9b677c729d363f66c40f5da81e0c90f89b140
Francisco De La Cruz <helloquico@gmail.com>
2015-09-12 18:46:51 -0400

    Stores - Change Listeners

---
+++ src/stores/authorStore.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change';
+
+var AuthorStore = assign({}, EventEmitter.prototype, {
+	addChangeListener: function(callback) {
+		this.on(CHANGE_EVENT, callback);
+	},
+
+	removeChangeListener: function(callback) {
+		this.removeListener(CHANGE_EVENT, callback);
+	},
+
+	emitChange: function() {
+		this.emit(CHANGE_EVENT);
+	}
+});
